cf8de9bffd4fde5f52d951329e6e7dbc350d1977
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2024-12-14 11:21:14 +0530

    chat deploy

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import './chat.css'
+import Loader from '../loader/loader'
 function chat() {
   function getdata() {
     var input = document.getElementById("input-medicine").value
-    fetch(`https://1e94-2405-201-1000-8077-ed61-e81b-7f46-38e.ngrok-free.app/med?query=${input}`)
+    document.getElementById("responseBox").style.display = "flex"
+    document.getElementById("respLoader").style.display = "flex"
+    document.getElementById("response").style.display = "none"
+    fetch(`https://medcheck-model.onrender.com/med?query=${input}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+      document.getElementById("response").style.display = "flex"
+      document.getElementById("respLoader").style.display = "none"
+      if (data.status == "no_match") {
+        document.getElementById("response").innerText = data.message
+      }
+      else if(data.status == "success"){
+        document.getElementById("response").innerText = 
+       `Medicine Name : ${data.medicine_name}
+        Uses : ${data.uses}
+        Composition : ${data.composition}
+        Side Effects : ${data.side_effects}
+        `
+      }
+    })
     .catch(err=>{console.log(err)})
+  }
+  const laoderStyle ={
+    width: "45px",
+    height: "45px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   }
   return (
     <div className='chatWrap'>
@@ -15,9 +40,10 @@ function chat() {
             <input type="text" name="medicine" id="input-medicine" placeholder='Enter your medicine name.' />
             <button id='search' onClick={()=>{getdata()}} >Search</button>
         </div>
-        <div className="responseBox">
+        <div id='responseBox' className="responseBox">
             <div className="chatLogo">H</div>
-            <div className="response">Hello, the closest match to your medicine is Crocin Advance 500mg Tablet , it is used for Pain relief, Treatment of Fever , and has the following side effects : Stomach pain Nausea Vomiting , here are the composition(s) of your medicine Paracetamol (500mg)</div>
+            <div id='respLoader' className="loader" style={laoderStyle} ><Loader></Loader></div>
+            <div id='response' className="response"></div>
         </div>
     </div>
   )
