7c42206c5876ba6a7a40fea208d56097f8b2145b
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2025-04-12 00:29:14 +0530

    emergency button added

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './nav.css'
 import menu from '../../assets/menu.svg'
 import cancle from '../../assets/cancle.svg'
+import emergency from '../../assets/emergency.svg'
 import { useNavigate } from "react-router-dom";
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -36,17 +37,17 @@ function Navbar() {
         </div>
         
         <div className={`center ${menuOpen ? 'active' : ''}`}>
-            <a href='#' onClick={() => isMobile && setMenuOpen(false)}>Services</a>
-            <a href='#' onClick={() => isMobile && setMenuOpen(false)}>How it Works</a>
-            <a href='#' onClick={() => isMobile && setMenuOpen(false)}>Contact</a>
-            <a href='#' onClick={() => isMobile && setMenuOpen(false)}>About</a>
+            <a href='#' onClick={() => isMobile && setMenuOpen(false)}> <span hidden={isMobile?false:true} >▢&nbsp;&nbsp;</span>  Services</a>
+            <a href='#' onClick={() => isMobile && setMenuOpen(false)}> <span hidden={isMobile?false:true} >▢&nbsp;&nbsp;</span>  How it Works</a>
+            <a href='#' onClick={() => isMobile && setMenuOpen(false)}> <span hidden={isMobile?false:true} >▢&nbsp;&nbsp;</span>  Contact</a>
+            <a href='#' onClick={() => isMobile && setMenuOpen(false)}> <span hidden={isMobile?false:true} >▢&nbsp;&nbsp;</span>  About</a>
         </div>
         
         <div className="right">
             <button id='login'>Login</button>
             <button id='emergency'>Emergency</button>
         </div>
-        
+        <div className="emergencyIcon" hidden={isMobile?false:true}><img src={emergency} alt="" /></div>
         <button className="mobile-menu-button" onClick={toggleMenu}>
           {menuOpen ? <><img src={cancle} alt="X" /></> : <><img src={menu} alt="X" /></>}
         </button>
